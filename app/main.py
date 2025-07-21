cf2a7921a1eb8fbed05c7d30988c64cfd750173e
MuhammadHaweras <haweras.noor@gmail.com>
2025-07-22 01:58:15 +0500

    Posts CRUD with SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,100 +1,85 @@
-from fastapi import FastAPI, Response, status, HTTPException
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from typing import List
 import pymysql
 from pymysql.cursors import DictCursor
+from . import models, schemas
 import time
 
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
-
-# -------------------- Database Connection --------------------
-
-while True:
-    try:
-        conn = pymysql.connect(
-            host="localhost",
-            user="root",
-            password="root",
-            database="fastapi",
-            cursorclass=DictCursor
-        )
-        cursor = conn.cursor()
-        print("✅ Connected to MySQL")
-        break
-    except Exception as e:
-        print("❌ Error connecting to DB:", e)
-        time.sleep(3)
-
-# -------------------- Pydantic Schema --------------------
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
-
-# -------------------- Helper Functions --------------------
-
-def get_post_by_id(post_id: int):
-    cursor.execute("SELECT * FROM posts WHERE id = %s", (post_id,))
-    return cursor.fetchone()
 
 # -------------------- Routes --------------------
 
 @app.get("/")
+
 async def root():
     return {"message": "Hello, World!!!!!!"}
 
 # Get all posts
-@app.get("/posts")
-def get_posts():
-    cursor.execute("SELECT * FROM posts")
-    return cursor.fetchall()
+@app.get("/posts", response_model= List[schemas.Post])
+
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+
+    return posts
 
 # Create new post
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(new_post: Post):
-    cursor.execute(
-        "INSERT INTO posts (title, content, published) VALUES (%s, %s, %s)",
-        (new_post.title, new_post.content, new_post.published)
-    )
-    conn.commit()
-    post_id = cursor.lastrowid
-    return get_post_by_id(post_id)
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model= schemas.Post)
+
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    
+    return new_post
 
 # Get single post by ID
-@app.get("/posts/{id}")
-def get_post(id: int):
-    post = get_post_by_id(id)
+
+@app.get("/posts/{id}", response_model= schemas.Post)
+
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
     if not post:
         raise HTTPException(status_code=404, detail=f"Post with id {id} not found")
+
     return post
 
 # Delete a post
+
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    post = get_post_by_id(id)
-    if not post:
+
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if not post.first():
         raise HTTPException(status_code=404, detail=f"Post with id {id} not found")
 
-    cursor.execute("DELETE FROM posts WHERE id = %s", (id,))
-    conn.commit()
+    post.delete(synchronize_session=False)
+
+    db.commit()
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # Update a post
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post):
-    post = get_post_by_id(id)
-    if not post:
+
+@app.put("/posts/{id}", response_model= schemas.Post)
+
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if not post.first():
         raise HTTPException(status_code=404, detail=f"Post with id {id} not found")
 
-    cursor.execute(
-        """
-        UPDATE posts
-        SET title = %s, content = %s, published = %s
-        WHERE id = %s
-        """,
-        (updated_post.title, updated_post.content, updated_post.published, id)
-    )
-    conn.commit()
-    return get_post_by_id(id)
+    post.update(updated_post.dict(), synchronize_session=False)
+
+    db.commit()
+
+    return post.first()
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, Text
+from sqlalchemy.sql import expression
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from .database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    title = Column(String(255), nullable=False)
+    content = Column(Text, nullable=False)
+    published = Column(Boolean, server_default=expression.true())
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text('now()'))
