3af7b42f521dbe5da9d064e3db7df4fc3c793a31
MuhammadHaweras <haweras.noor@gmail.com>
2025-07-26 22:34:05 +0500

    fix User model

--- app/models.py
+++ app/models.py
@@ -5,27 +5,27 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.orm import relationship
 from .database import Base
 
-# class Post(Base):
-#     __tablename__ = "post"
+class Post(Base):
+    __tablename__ = "post"
 
-#     id = Column(Integer, primary_key=True, index=True, nullable=False)
-#     title = Column(String(255), nullable=False)
-#     content = Column(Text, nullable=False)
-#     published = Column(Boolean, server_default=expression.true())
-#     created_at = Column(TIMESTAMP(timezone=True), nullable=False,
-#                         server_default=text('now()'))
-#     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"),nullable=False)
-#     user = relationship("User")
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    title = Column(String(255), nullable=False)
+    content = Column(Text, nullable=False)
+    published = Column(Boolean, server_default=expression.true())
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text('now()'))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"),nullable=False)
+    user = relationship("User")
 
 
-# class User(Base):
-#     __tablename__ = "users"
+class User(Base):
+    __tablename__ = "users"
 
-#     id = Column(Integer, primary_key=True, index=True, nullable=False)
-#     email = Column(String(255), unique=True, nullable=False)
-#     password = Column(String(255), nullable=False)
-#     created_at = Column(TIMESTAMP(timezone=True), nullable=False,
-#                         server_default=text('now()'))
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text('now()'))
 
 class Vote(Base):
     __tablename__ = "votes"
